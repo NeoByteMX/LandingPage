97cc44174abbb0d7c2e67a506b13fb332d42d0ab
Edwin <mmeo1107@gmail.com>
2025-10-07 14:55:43 -0600

    las imagenes se de los proyectos se cargan ahora en media, agregada la ruta de Media Files en settings, y en models, variable image

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media files
-MEDIA_URL= "/media"
-MEDIA_ROOT = Path(BASE_DIR,"/media")
+MEDIA_URL= "/media/"
+MEDIA_ROOT = BASE_DIR / 'media'
